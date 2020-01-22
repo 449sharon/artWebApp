1bf16e8e33a6143e121c984bad5085d0655adf5a
Msi87 <51952763+Msi87@users.noreply.github.com>
2020-01-22 16:37:55 +0200

    bb

--- src/app/pages/categorylist/categorylist.page.ts
+++ src/app/pages/categorylist/categorylist.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { ViewProductDetailsPage } from '../view-product-details/view-product-details.page';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-categorylist',
@@ -10,15 +11,36 @@ import { ViewProductDetailsPage } from '../view-product-details/view-product-det
 })
 export class CategorylistPage implements OnInit {
 
+  db = firebase.firestore();
+
+  Products = [];
+  myProduct = false;
   constructor(private router: Router,  public modalController: ModalController, ) { }
 
   ngOnInit() {
+    this.getProduct();
   }
 
-  // viewProductDetails(){
-  //   this.router.navigateByUrl('/view-product-details');
-  // }
- 
+  getProduct(){
+    let obj = {id : '', obj : {}};
+  this.db.collection('Products').get().then(snapshot => {
+    this.Products = [];
+    if (snapshot.empty) {
+            this.myProduct = false;
+          } else {
+            this.myProduct = true;
+            snapshot.forEach(doc => {
+              obj.id = doc.id;
+              obj.obj = doc.data();
+              this.Products.push(obj);
+              obj = {id : '', obj : {}};
+            });
+            return this.Products;
+            console.log("details", this.Products);
+            
+          }
+  });
+  }
 
   async createViewProduct() {
     const modal = await this.modalController.create({
