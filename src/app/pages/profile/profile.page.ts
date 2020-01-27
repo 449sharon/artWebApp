76686020d7a310e4a44df26fcfe525ec406c1e9c
449sharon <50619633+449sharon@users.noreply.github.com>
2020-01-27 16:54:26 +0200

    updates

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -6,199 +6,153 @@ import { AlertController, PopoverController, NavParams } from '@ionic/angular';
 import { Router, NavigationExtras } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { TrackOrderPage } from '../track-order/track-order.page';
-
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  db = firebase.firestore();
-  storage = firebase.storage().ref();
   @ViewChild('cart', {static: false, read: ElementRef})fab: ElementRef;
   cartItemCount: BehaviorSubject<number>;
+  db = firebase.firestore();
+  storage = firebase.storage().ref();
   uid
+  customerUid = firebase.auth().currentUser.uid;
+  dbOrder = firebase.firestore().collection('Order');
   profile = {
     image: '',
     name: '',
+    number:'',
     address: '',
-    uid: '',
-    number: '',
+  
     email: firebase.auth().currentUser.email,
+   
+    uid: '',
+    
   }
-
-
-  ///////
-  email
-  name
-  number
-  address
-  image
-  /////////
   Allorders = [];
-  cart = [];
+  loader: boolean = true;
   uploadprogress = 0;
   errtext = '';
   isuploading = false;
   isuploaded = false;
   isprofile = false;
-  Users = {
+  admin = {
     uid: '',
-    email: '',
+    email:''
   }
-
-/////////
-
-  customerUid = firebase.auth().currentUser.uid;
-  dbOrder = firebase.firestore().collection('Order');
-
-////////
-  public item =[];
-  conArray = []
-  Orders =[]
-  public display;
-  public postSort='recent';
-  public userID;
-  public userTransact: any;
-  users = [];
-  myArray =[]
-  constructor(public modalController: ModalController,
-    public router: Router,
-    public alertCtrl: AlertController,
-    public popoverController: PopoverController,
-    // public data: ProductService,
-    // public transact: TransactionService,
-    // private cartService: CartService
-    ) { 
-      this.uid = firebase.auth().currentUser.uid;
-    }
-
+  
+  constructor(public alertCtrl: AlertController,
+    private router: Router,
+    public modalController: ModalController
+   ) { 
+    this.uid = firebase.auth().currentUser.uid;
+    
+  }
+  ionViewWillEnter() {
+    setTimeout(() => {
+      this.loader = false;
+    }, 2000);
+  }
+ 
   ngOnInit() {
-    firebase.auth().onAuthStateChanged(Users => {
-      if (Users) {
-        this.Users.uid = Users.uid
-        this.Users.email =Users.email
-      this.getProfile(); 
-       this.GetOrders();
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log('Got admin', user);
+        this.admin.uid = user.uid
+        this.admin.email = user.email
+        this.GetOrders();
+      this.getProfile();
       } else {
-        console.log('no user');
+        console.log('no admin');
         
       }
     })
-    // this.cart = this.cartService.getCart();
-    // this.cartItemCount = this.cartService.getCartItemCount();
   }
-
-  changeListener(event): void {
-    const i = event.target.files[0];
-    console.log(i);
-    const upload = this.storage.child(i.name).put(i);
-    upload.on('state_changed', snapshot => {
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('upload is: ', progress , '% done.');
-    }, err => {
-    }, () => {
-      upload.snapshot.ref.getDownloadURL().then(dwnURL => {
-        console.log('File avail at: ', dwnURL);
-        this.image = dwnURL;
-      });
-    });
-  }
-
-
-async getImage(image){
-  let imagetosend = image.item(0);
-  if (!imagetosend) {
-    const imgalert = await this.alertCtrl.create({
-      message: 'Select image to upload',
-      buttons: [{
-        text: 'Okay',
-        role: 'cancel'
-      }]
-    });
-    imgalert.present();
-  } else {
-    if (imagetosend.type.split('/')[0] !== 'image') {
+  async getImage(image){
+    let imagetosend = image.item(0);
+    if (!imagetosend) {
       const imgalert = await this.alertCtrl.create({
-        message: 'Unsupported file type.',
+        message: 'Select image to upload',
         buttons: [{
           text: 'Okay',
           role: 'cancel'
         }]
       });
       imgalert.present();
-      imagetosend = '';
-      return;
-     } else {
-      const upload = this.storage.child(image.item(0).name).put(imagetosend);
-      upload.on('state_changed', snapshot => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        this.uploadprogress = progress;
-        this.isuploading = true;
-        if (progress==100){
-          this.isuploading = false;
-        } 
-      }, error => {
-      }, () => {
-        upload.snapshot.ref.getDownloadURL().then(downUrl => {this.ngOnInit
-          this.image = downUrl;
-          this.uploadprogress = 0;
-          this.isuploaded = true;
+    } else {
+      if (imagetosend.type.split('/')[0] !== 'image') {
+        const imgalert = await this.alertCtrl.create({
+          message: 'Unsupported file type.',
+          buttons: [{
+            text: 'Okay',
+            role: 'cancel'
+          }]
         });
-      });
-     }
+        imgalert.present();
+        imagetosend = '';
+        return;
+       } else {
+        const upload = this.storage.child(image.item(0).name).put(imagetosend);
+        upload.on('state_changed', snapshot => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          this.uploadprogress = progress;
+          this.isuploading = true;
+          if (progress==100){
+            this.isuploading = false;
+          } 
+        }, error => {
+        }, () => {
+          upload.snapshot.ref.getDownloadURL().then(downUrl => {this.ngOnInit
+            this.profile.image = downUrl;
+            this.uploadprogress = 0;
+            this.isuploaded = true;
+          });
+        });
+        this.loader
+       }
+    }
   }
-}
-
   createAccount(){
-// firebase.firestore().collection("UserProfile").doc(firebase.auth().currentUser.uid).update({
-//       name : this.profile.name,
-//       image : this.image, 
-//       number : this.profile.number,
-//       address : this.profile.address,
-//       email : firebase.auth().currentUser.email
-//     })
-    
-    if (!this.address||!this.name||!this.number){
-
+    if (!this.profile.address||!this.profile.name||!this.profile. number){
       this.errtext = 'Fields should not be empty'
     } else {
-      if (!this.image){
+      if (!this.profile.image){
         this.errtext = 'Profile image still uploading or not selected';
       } else {
-        this.profile.uid =  this.Users.uid;
-
+        this.profile.uid =  this.admin.uid;
         this.db.collection('UserProfile').doc(firebase.auth().currentUser.uid).set(this.profile).then(res => {
+          console.log('Profile created');
           this.getProfile();
         }).catch(error => {
           console.log('Error');
         });
-
       }
     }
   }
-  edit() {
-    this.isprofile = false;
-  }
-
-  getProfile(){
-    this.db.collection('UserProfile').where('uid', '==', this.Users.uid).get().then(snapshot => {
+   getProfile(){
+    this.db.collection('UserProfile').where('uid', '==', this.admin.uid).get().then(snapshot => {
       if (snapshot.empty) {
         this.isprofile = false;
       } else {
         this.isprofile = true;
-
         snapshot.forEach(doc => {
-          this.email = doc.data().email
-          this.profile.number = doc.data().number
-          this.profile.address = doc.data().address
-          this.profile.name = doc.data().name
-          this.profile.image = doc.data().image
+          this.profile.address = doc.data().address;
+          this.profile.image= doc.data().image
+          this.profile.name=doc.data().name
+        
+          this.profile.number=doc.data().number
+          this.profile.email=doc.data().email
+          
         })
       }
     })
+    ////
+    //////
   }
-
+  edit() {
+    this.isprofile = false;
+  }
      ////////////////////////////////// 
      GetOrders(){
       this.dbOrder.where('userID','==',firebase.auth().currentUser.uid).onSnapshot((data)=>{
